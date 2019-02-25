89b5d6686c6ea2ebf743b8cb6262d8f249ca8f24
MatthewAmes <mathoozala@gmail.com>
2019-02-25 11:01:29 -0700

    In class lecture  2/25/29

---
+++ First Game 1610/2nd Project/Assets/Conditionals.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conditionals : MonoBehaviour
+{
+
+	public bool CanWalk;
+	public int Number;
+	public string Password;
+
+	// Use this for initialization
+	void Start() {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!CanWalk)
+		{
+			print("True");
+		}
+		else
+		{
+			
+		}
+	}
+}
